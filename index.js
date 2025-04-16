79f5bdbaa9c1a2d69fd6c2835fd92b13d5f30900
oleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-04-16 19:58:18 +0200

    header and nav div added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // function to creat Index Page
 function renderIndexPage() {
     document.body.innerHTML = '';
-    const main = document.createElement('div');
-    main.textContent = 'Index page';
+    // document.body.style.backgroundImage = 'url("your-image-path.jpg")';
+
+    const main = document.createElement('main');
+    // main.textContent = 'Index page';
 
 // header
 const logo = document.createElement('div');
-logo.textContent = 'LOGO';
+logo.textContent = 'Weather App NL';
+logo.id = 'logo';
 
 
 
@@ -15,6 +18,8 @@ logo.textContent = 'LOGO';
 // Navigation bar
 const navDiv = document.createElement('div');
 navDiv.id = 'navDiv';
+const findButton = document.createElement('div');
+findButton.classList.add('button'); 
 const link = document.createElement('a');
 link.textContent = 'Find';
 link.href = '#';
@@ -27,7 +32,8 @@ link.addEventListener('click', (event) => {
 document.body.appendChild(main);
 main.appendChild(logo);
 document.body.appendChild(navDiv);
-navDiv.appendChild(link);
+navDiv.appendChild(findButton);
+findButton.appendChild(link);
 }
 
 
@@ -40,16 +46,19 @@ function renderResultPage() {
 document.body.innerHTML = '';
 
 const main = document.createElement('main');
-main.textContent = 'Result Page';
+// main.textContent = 'Result Page';
 
 // header
 const logo = document.createElement('div');
-logo.textContent = 'LOGO';
+logo.textContent = 'Weather App NL';
+logo.id = 'logo';
 
-
+// Navigation bar
 // Create link to Page 2
 const navDiv = document.createElement('div');
 navDiv.id = 'navDiv';
+const newSeachButton = document.createElement('div');
+newSeachButton.classList.add('button'); 
 const link = document.createElement('a');
 link.textContent = 'New Search';
 link.href = '#';
@@ -59,8 +68,8 @@ renderIndexPage();
 });
 
 // Create link to Page 3
-const navDiv2 = document.createElement('div');
-navDiv2.id = 'navDiv2';
+const extraPageButton = document.createElement('div');
+extraPageButton.classList.add('button'); 
 const link2 = document.createElement('a');
 link2.textContent = 'More Info';
 link2.href = '#';
@@ -72,9 +81,10 @@ link2.addEventListener('click', (event) => {
 document.body.appendChild(main);
 main.appendChild(logo);
 document.body.appendChild(navDiv);
-navDiv.appendChild(link);
-document.body.appendChild(navDiv2);
-navDiv2.appendChild(link2);
+navDiv.appendChild(newSeachButton);
+newSeachButton.appendChild(link);
+navDiv.appendChild(extraPageButton);
+extraPageButton.appendChild(link2);
 };
 
 
@@ -87,17 +97,22 @@ function renderExtraInfoPage() {
 document.body.innerHTML = '';
 
 const main = document.createElement('main');
-main.textContent = 'Extra Info';
+// main.textContent = 'Extra Info';
 
 // header
 const logo = document.createElement('div');
-logo.textContent = 'LOGO';
+logo.textContent = 'Weather App NL';
+logo.id = 'logo';
+const title = document.createElement('div');
+title.textContent = 'Extra Info';
+title.id = 'title';
 
-
-
+// Navigation bar
 // Create link to the Index Page
 const navDiv = document.createElement('div');
 navDiv.id = 'navDiv';
+const backButton = document.createElement('div');
+backButton.classList.add('button');  //    WHY IT DOES NOT WORK ?????
 const link = document.createElement('a');
 link.textContent = 'Back';
 link.href = '#';
@@ -106,8 +121,8 @@ link.addEventListener('click', (event) => {
     renderResultPage();
 });
 
-const navDiv2 = document.createElement('div');
-navDiv2.id = 'navDiv';
+const newSearchButton = document.createElement('div');
+newSearchButton.classList.add('button'); 
 const link2 = document.createElement('a');
 link2.textContent = 'New Search';
 link2.href = '#';
@@ -118,10 +133,12 @@ renderIndexPage();
 
 document.body.appendChild(main);
 main.appendChild(logo);
+main.appendChild(title);
 document.body.appendChild(navDiv);
-navDiv.appendChild(link);
-document.body.appendChild(navDiv2);
-navDiv2.appendChild(link2);
+navDiv.appendChild(backButton);
+backButton.appendChild(link);
+navDiv.appendChild(newSearchButton);
+newSearchButton.appendChild(link2);
 }
 
 // Initialize with Page 1

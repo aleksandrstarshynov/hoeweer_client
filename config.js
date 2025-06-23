b4b191ddab7ccb1238b48bcb10a6e90e4b73cead
aleksandrstarshynov <aleksandrstarshynov@gmail.com>
2025-06-23 11:15:36 +0200

    env fix

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||  // for Netlify
-  'http://localhost:4000';              // if local
+export const API_BASE_URL = 'https://hoeweer-server.onrender.com';
